77041d55d62a21a03a250cf6da052c459cc174c2
BurakUmutAlkan <burumalkan96@gmail.com>
2022-12-28 17:15:24 +0300

    Add project files.

---
+++ WinFormOopPractice/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace WinFormOopPractice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.nudAge = new System.Windows.Forms.NumericUpDown();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAge)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(12, 9);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(86, 21);
+            this.lblFirstName.TabIndex = 0;
+            this.lblFirstName.Text = "First Name";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(165, 208);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(368, 54);
+            this.btnCreate.TabIndex = 3;
+            this.btnCreate.Text = "Create a New Person";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(16, 33);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(228, 29);
+            this.txtFirstName.TabIndex = 0;
+            // 
+            // nudAge
+            // 
+            this.nudAge.Location = new System.Drawing.Point(552, 33);
+            this.nudAge.Name = "nudAge";
+            this.nudAge.Size = new System.Drawing.Size(120, 29);
+            this.nudAge.TabIndex = 2;
+            this.nudAge.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(548, 9);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(37, 21);
+            this.lblAge.TabIndex = 0;
+            this.lblAge.Text = "Age";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(283, 9);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(84, 21);
+            this.lblLastName.TabIndex = 0;
+            this.lblLastName.Text = "Last Name";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblOutput.Location = new System.Drawing.Point(16, 74);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(656, 131);
+            this.lblOutput.TabIndex = 0;
+            this.lblOutput.Text = "Example Text";
+            this.lblOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(287, 33);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(228, 29);
+            this.txtLastName.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 271);
+            this.Controls.Add(this.nudAge);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblFirstName);
+            this.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OOP Practice";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.nudAge)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.NumericUpDown nudAge;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.TextBox txtLastName;
+    }
+}
+
---
+++ WinFormOopPractice/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormOopPractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ClearForm();
+        }
+
+        #region Methods
+
+        private void ClearForm()
+        {
+            txtFirstName.Clear();
+            txtLastName.Clear();
+            nudAge.Value = 0;
+            lblOutput.Text = string.Empty;
+        }
+
+        private Person CreateNewPerson()
+        {
+            string firstName = txtFirstName.Text.Trim();
+            string lastName = txtLastName.Text.Trim();
+            int age = Convert.ToInt32(nudAge.Value);
+
+            Person createdPerson = new Person()
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Age = age,
+            };
+
+            return createdPerson;
+        }
+
+        private void PrintPersonInfo(Person createdPerson)
+        {
+            ClearForm();
+            lblOutput.Text = $"First Name: {createdPerson.FirstName}\nLast Name: {createdPerson.LastName}\nAge: {createdPerson.Age}";
+        }
+
+        #endregion
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            PrintPersonInfo(CreateNewPerson());
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+
+    #region Classes
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+
+    #endregion
+}
